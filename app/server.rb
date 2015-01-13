e6f35db01d6acae606a78b9b0766659941075766
Josh Bebbington <josh@focalwebsites.com>
2015-01-13 12:10:56 +0000

    Setup sample tests for datamapper

--- app/server.rb
+++ app/server.rb
@@ -1,5 +1,17 @@
 require 'sinatra/base'
+require 'data_mapper'
 
 class BookmarkManager < Sinatra::Base
+
+  env = ENV['RACK_ENV'] || 'development'
+
+  DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+  require_relative '../lib/link.rb'
+  DataMapper.finalize
+  DataMapper.auto_upgrade!
+
+  get '/' do
+    erb :index
+  end
 
 end
