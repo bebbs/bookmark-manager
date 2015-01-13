0bf207da1aa3391d6110828978da7df56eaebac9
Josh Bebbington <josh@focalwebsites.com>
2015-01-13 18:00:56 +0000

    Users are welcomed when they sign in

--- app/server.rb
+++ app/server.rb
@@ -8,6 +8,9 @@ require_relative '../lib/user.rb'
 
 class BookmarkManager < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
   env = ENV['RACK_ENV'] || 'development'
 
   DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
@@ -41,8 +44,15 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users' do
-    User.create(:email => params[:email], :password => params[:password])
+    user = User.create(:email => params[:email], :password => params[:password])
+    session[:user_id] = user.id
     redirect '/'
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id]) if session[:user_id]
+    end
   end
 
 end
